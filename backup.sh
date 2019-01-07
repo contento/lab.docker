d5f283487f09056318a2398a8f49d9ee5084abd0
Ubuntu <developer@vm-gc-linux.druuumz0qrcuzc3fe1zo5tn1xe.bx.internal.cloudapp.net>
2019-01-07 11:19:46 -0500

    Improve backup script

--- backup.sh
+++ backup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-find ./ -path '*/.*' -prune -o -type f -print | zip ~/backup-$(date +%F-%H%M%S).zip -@ -x *.zip
+
+projectName=${PWD##*/}
+backupPath=~/backup/$projectName
+
+if [ ! -d  $backupPath ]; then
+  mkdir -p $backupPath
+fi
+
+find ./ -path '*/.*' -prune -o -type f -print | zip "$backupPath/$projectName-$(date +%F-%H.%M.%S).zip" -@ -x *.zip
